e12090fc3bd687e6dc41f5045f9c8800e5c017b4
luojia65 <me@luojia.cc>
2020-02-15 13:54:15 +0800

    Save changes

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,10 @@
 use libc::{c_char, c_void};
 use mach::kern_return;
+use core_foundation::{
+    base::{CFAllocatorRef},
+    uuid::{CFUUIDRef, CFUUIDBytes}
+};
+use core::ptr;
 
 extern "C" {
     pub static kIOMasterPortDefault: mach_port_t;
@@ -15,6 +20,20 @@ extern "C" {
         entry: io_registry_entry_t,
         name: *mut c_char,
     ) -> kern_return::kern_return_t;
+    pub fn IOCreatePlugInInterfaceForService(
+        service: io_service_t,
+        plugin_type: CFUUIDRef,
+        interface_type: CFUUIDRef,
+        the_interface: *mut *mut *mut IOCFPlugInInterface,
+        the_score: *mut i32,
+    ) -> kern_return::kern_return_t;
+    pub fn CFUUIDGetConstantUUIDWithBytes(
+        alloc: CFAllocatorRef,
+        byte0: UInt8, byte1: UInt8, byte2: UInt8, byte3: UInt8, 
+        byte4: UInt8, byte5: UInt8, byte6: UInt8, byte7: UInt8, 
+        byte8: UInt8, byte9: UInt8, byte10: UInt8, byte11: UInt8, 
+        byte12: UInt8, byte13: UInt8, byte14: UInt8, byte15: UInt8
+    ) -> CFUUIDRef;
 }
 
 #[allow(non_camel_case_types)]
@@ -25,11 +44,35 @@ pub type io_object_t = mach_port_t;
 pub type io_iterator_t = io_object_t;
 #[allow(non_camel_case_types)]
 pub type io_registry_entry_t = io_object_t;
+#[allow(non_camel_case_types)]
+pub type io_service_t = io_object_t;
 
 #[allow(non_snake_case)]
 pub fn kIOUSBDeviceClassName() -> *const c_char {
     b"IOUSBDevice\0".as_ptr() as *const c_char
 }
+#[allow(non_snake_case)]
+pub fn kIOUSBDeviceUserClientTypeID() -> CFUUIDRef {
+    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(), 
+        0x9d, 0xc7, 0xb7, 0x80, 0x9e, 0xc0, 0x11, 0xD4,
+        0xa5, 0x4f, 0x00, 0x0a, 0x27, 0x05, 0x28, 0x61
+    ) }
+}
+#[allow(non_snake_case)]
+pub fn kIOCFPlugInInterfaceID() -> CFUUIDRef {
+    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(),     
+        0xC2, 0x44, 0xE8, 0x58, 0x10, 0x9C, 0x11, 0xD4,
+        0x91, 0xD4, 0x00, 0x50, 0xE4, 0xC6, 0x42, 0x6F
+    ) }
+} 
+// macOS 10.14 & up
+#[allow(non_snake_case)] 
+pub fn kIOUSBInterfaceInterfaceID942() -> CFUUIDRef {
+    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(),     
+        0x87, 0x52, 0x66, 0x3B, 0xC0, 0x7B, 0x4B, 0xAE,
+        0x95, 0x84, 0x22, 0x03, 0x2F, 0xAB, 0x9C, 0x5A
+    ) }
+} 
 
 #[repr(C)]
 pub struct __CFDictionary {
@@ -37,3 +80,42 @@ pub struct __CFDictionary {
 }
 pub type CFMutableDictionaryRef = *mut __CFDictionary;
 pub type CFDictionaryRef = *const __CFDictionary;
+
+pub type UInt8 = u8;
+pub type UInt16 = u16;
+pub type UInt32 = u32;
+pub type SInt32 = i32;
+pub type REFIID = CFUUIDBytes;
+pub type LPVOID = *const void::Void; // for FFI-safe
+pub type HRESULT = SInt32;
+pub type ULONG = UInt32;
+pub type IOReturn = kern_return::kern_return_t;
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOCFPlugInInterface {
+    // IUNKNOWN_C_GUTS
+    pub _reserved: *const Self,
+    pub QueryInterface: extern "C" fn(
+        this: *mut *mut Self, // todo: verify
+        iid: REFIID, 
+        ppv: *mut LPVOID
+    ) -> HRESULT,
+    pub AddRef: extern "C" fn(this: *mut Self) -> ULONG,
+    pub Release: extern "C" fn(this: *mut *mut Self) -> ULONG,
+    // IOCFPLUGINBASE
+    pub version: UInt16,
+    pub revision: UInt16,
+    pub Probe: extern "C" fn(
+        this: *mut Self, 
+        property_table: CFDictionaryRef, 
+        service: io_service_t,
+        order: *mut SInt32
+    ) -> IOReturn,
+    pub Start: extern "C" fn(
+        this: *mut Self,
+        property_table: CFDictionaryRef, 
+        service: io_service_t
+    ) -> IOReturn,
+    pub Stop: extern "C" fn(this: *mut Self) -> IOReturn,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod ffi;
 use core::mem::MaybeUninit;
 use std::ffi::CStr;
 use ffi::*;
+use core_foundation::uuid::CFUUIDGetUUIDBytes;
 
 fn main() {
     let classes_to_match = unsafe { IOServiceMatching(kIOUSBDeviceClassName()) };
@@ -32,6 +33,7 @@ fn main() {
             break
         }
         dbg!(next);
+
         let mut name = vec![0i8; 1024];
         let kr = unsafe { IORegistryEntryGetName(
             next, 
@@ -39,10 +41,40 @@ fn main() {
         ) };
         if kr != mach::kern_return::KERN_SUCCESS {
             println!("IORegistryEntryGetName not success!");
-            return;
+            continue;
         }
         let name = unsafe { CStr::from_ptr(name.as_ptr()) };
         dbg!(name);
+
+        let mut plugin_interface: *mut *mut IOCFPlugInInterface 
+            = unsafe { MaybeUninit::uninit().assume_init() };
+        let mut score = unsafe { MaybeUninit::uninit().assume_init() };
+        let kr = unsafe { IOCreatePlugInInterfaceForService(
+            next,
+            kIOUSBDeviceUserClientTypeID(),
+            kIOCFPlugInInterfaceID(),
+            &mut plugin_interface as *mut _,
+            &mut score as *mut _,
+        ) };
+        if kr != mach::kern_return::KERN_SUCCESS {
+            println!("IOCreatePlugInInterfaceForService not success! {}", kr);
+            unsafe { IOObjectRelease(next) };
+            continue;
+        } 
+
+        let mut device = unsafe { MaybeUninit::uninit().assume_init() };
+        unsafe { 
+            ((**plugin_interface).QueryInterface)(
+                plugin_interface, 
+                CFUUIDGetUUIDBytes(kIOUSBInterfaceInterfaceID942()), 
+                &mut device as *mut _ as *mut _
+            );
+        }
+        dbg!(device);
+        unsafe {
+            ((**plugin_interface).Release)(plugin_interface)
+        };
+
         unsafe { IOObjectRelease(next) };
     }
 
