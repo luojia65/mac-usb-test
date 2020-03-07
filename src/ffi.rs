47f42aa2469ccb00dde7b9546f7fd3115c8f8736
luojia65 <me@luojia.cc>
2020-03-07 12:21:47 +0800

    Save ffi changes

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,11 +1,11 @@
 #![allow(unused)]
+use core::ptr;
+use core_foundation::{
+    base::CFAllocatorRef,
+    uuid::{CFUUIDBytes, CFUUIDRef},
+};
 use libc::{c_char, c_void};
 use mach::kern_return;
-use core_foundation::{
-    base::{CFAllocatorRef},
-    uuid::{CFUUIDRef, CFUUIDBytes}
-};
-use core::ptr;
 
 extern "C" {
     pub static kIOMasterPortDefault: mach_port_t;
@@ -19,7 +19,7 @@ extern "C" {
     pub fn IOObjectRelease(object: io_object_t) -> kern_return::kern_return_t;
     pub fn IORegistryEntryGetName(
         entry: io_registry_entry_t,
-        name: *mut c_char,
+        name: io_name_t,
     ) -> kern_return::kern_return_t;
     pub fn IOCreatePlugInInterfaceForService(
         service: io_service_t,
@@ -33,10 +33,22 @@ extern "C" {
     ) -> kern_return::kern_return_t;
     pub fn CFUUIDGetConstantUUIDWithBytes(
         alloc: CFAllocatorRef,
-        byte0: UInt8, byte1: UInt8, byte2: UInt8, byte3: UInt8, 
-        byte4: UInt8, byte5: UInt8, byte6: UInt8, byte7: UInt8, 
-        byte8: UInt8, byte9: UInt8, byte10: UInt8, byte11: UInt8, 
-        byte12: UInt8, byte13: UInt8, byte14: UInt8, byte15: UInt8
+        byte0: UInt8,
+        byte1: UInt8,
+        byte2: UInt8,
+        byte3: UInt8,
+        byte4: UInt8,
+        byte5: UInt8,
+        byte6: UInt8,
+        byte7: UInt8,
+        byte8: UInt8,
+        byte9: UInt8,
+        byte10: UInt8,
+        byte11: UInt8,
+        byte12: UInt8,
+        byte13: UInt8,
+        byte14: UInt8,
+        byte15: UInt8,
     ) -> CFUUIDRef;
 }
 
@@ -50,6 +62,8 @@ pub type io_iterator_t = io_object_t;
 pub type io_registry_entry_t = io_object_t;
 #[allow(non_camel_case_types)]
 pub type io_service_t = io_object_t;
+#[allow(non_camel_case_types)]
+pub type io_name_t = *mut c_char; // [c_char; 128]
 
 #[allow(non_snake_case)]
 pub fn kIOUSBDeviceClassName() -> *const c_char {
@@ -57,9 +71,9 @@ pub fn kIOUSBDeviceClassName() -> *const c_char {
 }
 #[allow(non_snake_case)]
 pub fn kIOUSBDeviceUserClientTypeID() -> CFUUIDRef {
-    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(), 
+    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(),
         0x9d, 0xc7, 0xb7, 0x80, 0x9e, 0xc0, 0x11, 0xD4,
-        0xa5, 0x4f, 0x00, 0x0a, 0x27, 0x05, 0x28, 0x61
+        0xa5, 0x4f, 0x00, 0x0a, 0x27, 0x05, 0x28, 0x61,
     ) }
 }
 #[allow(non_snake_case)]
@@ -70,11 +84,18 @@ pub fn kIOCFPlugInInterfaceID() -> CFUUIDRef {
     ) }
 } 
 // macOS 10.14 & up
-#[allow(non_snake_case)] 
+#[allow(non_snake_case)]
 pub fn kIOUSBInterfaceInterfaceID942() -> CFUUIDRef {
     unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(),     
         0x87, 0x52, 0x66, 0x3B, 0xC0, 0x7B, 0x4B, 0xAE,
         0x95, 0x84, 0x22, 0x03, 0x2F, 0xAB, 0x9C, 0x5A
+    ) }
+} 
+#[allow(non_snake_case)]
+pub fn kIOUSBDeviceInterfaceID942() -> CFUUIDRef {
+    unsafe { CFUUIDGetConstantUUIDWithBytes(ptr::null(),
+        0x56, 0xAD, 0x08, 0x9D, 0x87, 0x8D, 0x4B, 0xEA,
+        0xA1, 0xF5, 0x2C, 0x8D, 0xC4, 0x3E, 0x8A, 0x98
     ) }
 } 
 
@@ -82,62 +103,240 @@ pub fn kIOUSBInterfaceInterfaceID942() -> CFUUIDRef {
 pub struct __CFDictionary {
     __private: c_void,
 }
+#[repr(C)]
+pub struct __CFRunLoopSource {
+    __private: c_void,
+}
 pub type CFMutableDictionaryRef = *mut __CFDictionary;
 pub type CFDictionaryRef = *const __CFDictionary;
+pub type CFRunLoopSourceRef = *mut __CFRunLoopSource;
 
 pub type UInt8 = u8;
 pub type UInt16 = u16;
 pub type UInt32 = u32;
+pub type UInt64 = u64;
 pub type SInt32 = i32;
+pub type Boolean = bool;
 pub type REFIID = CFUUIDBytes;
 pub type LPVOID = *const c_void;
 pub type HRESULT = SInt32;
 pub type ULONG = UInt32;
 pub type IOReturn = kern_return::kern_return_t;
 pub type USBDeviceAddress = UInt16;
+pub type AbsoluteTime = UInt64;
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOUSBConfigurationDescriptor {
+    pub bLength: u8,
+    pub bDescriptorType: u8,
+    pub wTotalLength: u16,
+    pub bNumInterfaces: u8,
+    pub bConfigurationValue: u8,
+    pub iConfiguration: u8,
+    pub bmAttributes: u8,
+    pub MaxPower: u8,
+}
+
+pub type IOUSBConfigurationDescriptorPtr = *mut IOUSBConfigurationDescriptor;
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOUSBDevRequest {
+    pub bmRequestType: u8,
+    pub bRequest: u8,
+    pub wValue: u16,
+    pub wIndex: u16,
+    pub wLength: u16,
+    pub pData: *const c_void,
+    pub wLenDone: u32,
+}
+
+pub type IOUSBDeviceRequestPtr = *mut IOUSBDevRequest;
+
+#[cfg(target_endian = "big")]
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct NumVersion {
+    pub majorRev: u8,
+    pub minorAndBugRev: u8,
+    pub stage: u8,
+    pub nonRelRev: u8,
+}
+
+#[cfg(target_endian = "little")]
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct NumVersion {
+    pub nonRelRev: u8,
+    pub stage: u8,
+    pub minorAndBugRev: u8,
+    pub majorRev: u8,
+}
+
+pub type IOAsyncCallback1 = fn(refcon: *const c_void, result: IOReturn, arg0: *const c_void);
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOUSBDevRequestTO {
+    /// Request type: kUSBStandard, kUSBClass or kUSBVendor
+    pub bmRequestType: u8,
+    /// Request code
+    pub bRequest: u8,
+    /// 16 bit parameter for request, host endianess
+    pub wValue: u16,
+    /// 16 bit parameter for request, host endianess
+    pub wIndex: u16,
+    /// Length of data part of request, 16 bits, host endianess
+    pub wLength: u16,
+    /// Pointer to data for request - data returned in bus endianess
+    pub pData: *const c_void,
+    /// Set by standard completion routine to number of data bytes actually transferred
+    pub wLenDone: u32,
+    /// Specifies a time value in milliseconds. Once the request is queued on the bus, 
+    /// if no data is transferred in this amount of time, the request will be aborted and returned.
+    pub noDataTimeout: u32,
+    /// Specifies a time value in milliseconds. Once the request is queued on the bus, 
+    /// if the entire request is not completed in this amount of time, the request will be aborted and
+    /// returned.
+    pub completionTimeout: u32,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOUSBFindInterfaceRequest {
+    /// requested class
+    pub bInterfaceClass: u16,
+    /// requested subclass            
+    pub bInterfaceSubClass: u16,
+    /// requested protocol        
+    pub bInterfaceProtocol: u16,
+    /// requested alt setting
+    pub bAlternateSetting: u16,
+}
 
 #[repr(C)]
 #[allow(non_snake_case)]
 pub struct IOCFPlugInInterface {
     // IUNKNOWN_C_GUTS
-    pub _reserved: *const Self,
-    pub QueryInterface: extern "C" fn(
-        this: *mut *mut Self, // todo: verify
-        iid: REFIID, 
-        ppv: *mut LPVOID
-    ) -> HRESULT,
+    pub _reserved: *const c_void,
+    pub QueryInterface: extern "C" fn(this: *mut *mut Self, iid: REFIID, ppv: *mut LPVOID) -> HRESULT,
     pub AddRef: extern "C" fn(this: *mut Self) -> ULONG,
     pub Release: extern "C" fn(this: *mut *mut Self) -> ULONG,
     // IOCFPLUGINBASE
     pub version: UInt16,
     pub revision: UInt16,
     pub Probe: extern "C" fn(
-        this: *mut Self, 
-        property_table: CFDictionaryRef, 
+        this: *mut Self,
+        property_table: CFDictionaryRef,
         service: io_service_t,
-        order: *mut SInt32
+        order: *mut SInt32,
     ) -> IOReturn,
     pub Start: extern "C" fn(
         this: *mut Self,
-        property_table: CFDictionaryRef, 
-        service: io_service_t
+        property_table: CFDictionaryRef,
+        service: io_service_t,
     ) -> IOReturn,
     pub Stop: extern "C" fn(this: *mut Self) -> IOReturn,
 }
 
 #[repr(C)]
 #[allow(non_snake_case)]
-pub struct IOUSBDeviceStruct942
-{
+pub struct IOUSBDeviceInterface942 {
     // IUNKNOWN_C_GUTS
-    pub _reserved: *const Self,
-    pub QueryInterface: extern "C" fn(
-        this: *mut *mut Self, // todo: verify
-        iid: REFIID, 
-        ppv: *mut LPVOID
-    ) -> HRESULT,
+    pub _reserved: *const c_void,
+    pub QueryInterface: extern "C" fn(this: *mut *mut Self, iid: REFIID, ppv: *mut LPVOID) -> HRESULT,
+    pub AddRef: extern "C" fn(this: *mut Self) -> ULONG,
+    pub Release: extern "C" fn(this: *mut *mut Self) -> ULONG,
+    // todo
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct IOUSBDeviceStruct942 {
+    // IUNKNOWN_C_GUTS
+    pub _reserved: *const c_void,
+    pub QueryInterface: extern "C" fn(this: *mut *mut Self, iid: REFIID, ppv: *mut LPVOID) -> HRESULT,
     pub AddRef: extern "C" fn(this: *mut Self) -> ULONG,
     pub Release: extern "C" fn(this: *mut *mut Self) -> ULONG,
     // IOUSBDeviceStruct942
-
+    pub CreateDeviceAsyncEventSource:
+        extern "C" fn(this: *mut Self, source: *mut CFRunLoopSourceRef) -> IOReturn,
+    pub GetDeviceAsyncEventSource: extern "C" fn(this: *mut Self) -> CFRunLoopSourceRef,
+    pub CreateDeviceAsyncPort: extern "C" fn(this: *mut Self, port: *mut mach_port_t) -> IOReturn,
+    pub GetDeviceAsyncPort: extern "C" fn(this: *mut Self) -> mach_port_t,
+    pub USBDeviceOpen: extern "C" fn(this: *mut Self) -> IOReturn,
+    pub USBDeviceClose: extern "C" fn(this: *mut Self) -> IOReturn,
+    pub GetDeviceClass: extern "C" fn(this: *mut Self, devClass: *mut UInt8) -> IOReturn,
+    pub GetDeviceSubClass: extern "C" fn(this: *mut Self, devSubClass: *mut UInt8) -> IOReturn,
+    pub GetDeviceProtocol: extern "C" fn(this: *mut Self, devProtocol: *mut UInt8) -> IOReturn,
+    pub GetDeviceVendor: extern "C" fn(this: *mut Self, devVendor: *mut UInt16) -> IOReturn,
+    pub GetDeviceProduct: extern "C" fn(this: *mut Self, devProduct: *mut UInt16) -> IOReturn,
+    pub GetDeviceReleaseNumber: extern "C" fn(this: *mut Self, devRelNum: *mut UInt16) -> IOReturn,
+    pub GetDeviceAddress: extern "C" fn(this: *mut Self, addr: *mut USBDeviceAddress) -> IOReturn,
+    pub GetDeviceBusPowerAvailable:
+        extern "C" fn(this: *mut Self, powerAvailable: *mut UInt32) -> IOReturn,
+    pub GetDeviceSpeed: extern "C" fn(this: *mut Self, devSpeed: *mut UInt8) -> IOReturn,
+    pub GetNumberOfConfigurations:
+        extern "C" fn(this: *mut Self, numConfig: *mut UInt8) -> IOReturn,
+    pub GetLocationID: extern "C" fn(this: *mut Self, locationID: *mut UInt32) -> IOReturn,
+    pub GetConfigurationDescriptorPtr: extern "C" fn(
+        this: *mut Self,
+        configIndex: UInt8,
+        desc: *mut IOUSBConfigurationDescriptorPtr,
+    ) -> IOReturn,
+    pub GetConfiguration: extern "C" fn(this: *mut Self, configNum: *mut UInt8) -> IOReturn,
+    pub SetConfiguration: extern "C" fn(this: *mut Self, configNum: UInt8) -> IOReturn,
+    pub GetBusFrameNumber:
+        extern "C" fn(this: *mut Self, frame: *mut UInt64, atTime: *mut AbsoluteTime) -> IOReturn,
+    pub ResetDevice: extern "C" fn(this: *mut Self) -> IOReturn,
+    pub DeviceRequest: extern "C" fn(this: *mut Self, req: *mut IOUSBDevRequest) -> IOReturn,
+    pub DeviceRequestAsync: extern "C" fn(
+        this: *mut Self,
+        req: *mut IOUSBDevRequest,
+        callback: IOAsyncCallback1,
+        refCon: *mut c_void,
+    ) -> IOReturn,
+    pub CreateInterfaceIterator: extern "C" fn(
+        this: *mut Self,
+        req: *mut IOUSBFindInterfaceRequest,
+        iter: *mut io_iterator_t,
+    ) -> IOReturn,
+    pub USBDeviceOpenSeize: extern "C" fn(this: *mut Self) -> IOReturn,
+    pub DeviceRequestTO: extern "C" fn(this: *mut Self, req: *mut IOUSBDevRequestTO) -> IOReturn,
+    pub DeviceRequestAsyncTO: extern "C" fn(
+        this: *mut Self,
+        req: *mut IOUSBDevRequestTO,
+        callback: IOAsyncCallback1,
+        refCon: *mut c_void,
+    ) -> IOReturn,
+    pub USBDeviceSuspend: extern "C" fn(this: *mut Self, suspend: Boolean) -> IOReturn,
+    pub USBDeviceAbortPipeZero: extern "C" fn(this: *mut Self) -> IOReturn,
+    pub USBGetManufacturerStringIndex: extern "C" fn(this: *mut Self, msi: *mut UInt8) -> IOReturn,
+    pub USBGetProductStringIndex: extern "C" fn(this: *mut Self, psi: *mut UInt8) -> IOReturn,
+    pub USBGetSerialNumberStringIndex: extern "C" fn(this: *mut Self, snsi: *mut UInt8) -> IOReturn,
+    pub USBDeviceReEnumerate: extern "C" fn(this: *mut Self, options: UInt32) -> IOReturn,
+    pub GetBusMicroFrameNumber: extern "C" fn(
+        this: *mut Self,
+        microFrame: *mut UInt64,
+        atTime: *mut AbsoluteTime,
+    ) -> IOReturn,
+    pub GetIOUSBLibVersion: extern "C" fn(
+        this: *mut Self,
+        ioUSBLibVersion: *mut NumVersion,
+        usbFamilyVersion: *mut NumVersion,
+    ) -> IOReturn,
+    pub GetBusFrameNumberWithTime:
+        extern "C" fn(this: *mut Self, frame: *mut UInt64, atTime: *mut AbsoluteTime) -> IOReturn,
+    pub GetUSBDeviceInformation: extern "C" fn(this: *mut Self, info: *mut UInt32) -> IOReturn,
+    pub RequestExtraPower: extern "C" fn(
+        this: *mut Self,
+        type_: UInt32,
+        requestedPower: UInt32,
+        powerAvailable: *mut UInt32,
+    ) -> IOReturn,
+    pub ReturnExtraPower:
+        extern "C" fn(this: *mut Self, type_: UInt32, powerReturned: UInt32) -> IOReturn,
+    pub GetExtraPowerAllocated:
+        extern "C" fn(this: *mut Self, type_: UInt32, powerAllocated: *mut UInt32) -> IOReturn,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod ffi;
 
 use core::mem::MaybeUninit;
-use std::ffi::CStr;
-use ffi::*;
 use core_foundation::uuid::CFUUIDGetUUIDBytes;
+use ffi::*;
+use std::ffi::CStr;
 
 fn main() {
     let matching_dict = unsafe { IOServiceMatching(kIOUSBDeviceClassName()) };
@@ -14,11 +14,9 @@ fn main() {
     }
 
     let mut iter: MaybeUninit<io_iterator_t> = MaybeUninit::uninit();
-    let kr = unsafe { IOServiceGetMatchingServices(
-        kIOMasterPortDefault,
-        matching_dict,
-        iter.as_mut_ptr()
-    ) };
+    let kr = unsafe {
+        IOServiceGetMatchingServices(kIOMasterPortDefault, matching_dict, iter.as_mut_ptr())
+    };
     dbg!(kr);
     if kr != mach::kern_return::KERN_SUCCESS {
         println!("IOServiceGetMatchingServices not success!");
@@ -36,34 +34,33 @@ fn my_get_usb_interface(iter: io_iterator_t) {
     loop {
         let service = unsafe { IOIteratorNext(iter) };
         if service == 0 {
-            break
+            break;
         }
         dbg!(service);
 
         //Create an intermediate plug-in
         let mut plugin_interface = MaybeUninit::uninit();
         let mut score = MaybeUninit::uninit();
-        let kr = unsafe { IOCreatePlugInInterfaceForService(
-            service,
-            kIOUSBDeviceUserClientTypeID(),
-            kIOCFPlugInInterfaceID(),
-            plugin_interface.as_mut_ptr(),
-            score.as_mut_ptr(),
-        ) };
+        let kr = unsafe {
+            IOCreatePlugInInterfaceForService(
+                service,
+                kIOUSBDeviceUserClientTypeID(),
+                kIOCFPlugInInterfaceID(),
+                plugin_interface.as_mut_ptr(),
+                score.as_mut_ptr(),
+            )
+        };
         // Don't need the device object after intermediate plug-in is created
         unsafe { IOObjectRelease(service) };
         if kr != mach::kern_return::KERN_SUCCESS {
             println!("IOCreatePlugInInterfaceForService not success! {}", kr);
             unsafe { IOObjectRelease(service) };
             continue;
-        } 
+        }
         dbg!(unsafe { score.assume_init() });
 
-        let mut name = vec![0i8; 1024];
-        let kr = unsafe { IORegistryEntryGetName(
-            service, 
-            name.as_mut_ptr()
-        ) };
+        let mut name = [0i8; 128];
+        let kr = unsafe { IORegistryEntryGetName(service, name.as_mut_ptr()) };
         if kr != mach::kern_return::KERN_SUCCESS {
             println!("IORegistryEntryGetName not success!");
             continue;
@@ -73,12 +70,12 @@ fn my_get_usb_interface(iter: io_iterator_t) {
 
         //Now create the device interface
         let plugin_interface = unsafe { plugin_interface.assume_init() };
-        let mut device = MaybeUninit::uninit();
-        let kr = unsafe { 
+        let mut device_interface = MaybeUninit::uninit();
+        let kr = unsafe {
             ((**plugin_interface).QueryInterface)(
-                plugin_interface, 
-                CFUUIDGetUUIDBytes(kIOUSBInterfaceInterfaceID942()), 
-                device.as_mut_ptr()
+                plugin_interface,
+                CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID942()),
+                device_interface.as_mut_ptr(),
             )
         };
         //Don't need the device object after intermediate plug-in is created
@@ -87,18 +84,15 @@ fn my_get_usb_interface(iter: io_iterator_t) {
             println!("QueryInterface not success! {}", kr);
             continue;
         }
-        dbg!(unsafe { device.assume_init() });
-        let device = unsafe { device.assume_init() };
+        dbg!(unsafe { device_interface.assume_init() });
+        let device_interface = unsafe { device_interface.assume_init() };
 
-        // get address 
+        // get address
         // let usb_device_address = MaybeUninit::uninit();
         // unsafe {
         //     (**device).GetDeviceAddress(device, usb_device_address.as_mut_ptr());
         // }
 
-
-        unsafe {
-            ((**plugin_interface).Release)(plugin_interface)
-        };
+        unsafe { ((**plugin_interface).Release)(plugin_interface) };
     }
 }
